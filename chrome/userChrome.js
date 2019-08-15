57bc7029e690b243ab205599a20c83e5c08fb212
cubetastic <cubetastic33@gmail.com>
2019-08-15 16:29:05 +0530

    Change placeholder URL

--- chrome/userChrome.js
+++ chrome/userChrome.js
@@ -8,7 +8,7 @@ https://opensource.org/licenses/MIT
 	if (location != 'chrome://browser/content/browser.xul') return;
 	// Set this to the URL of the webpage you want as your new tab page
 	// It can be a local file, like file:///path/to/file.html
-	// or a remote webpage, like https://start.duckduckgo.com
+	// or a remote webpage, like https://example.com
 	// or any other file you'd like
-	aboutNewTabService.newTabURL = 'file:///data/Documents/GitHub/new-tab/index.html';
+	aboutNewTabService.newTabURL = 'https://start.duckduckgo.com';
 })();
